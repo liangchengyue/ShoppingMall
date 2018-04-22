95bfdec58dacea2f37855413cac0f3ec352fbead
梁城月 <2524499314@qq.com>
2018-04-22 23:45:14 +0800

    Type的一对多，多对一映射

--- src/main/src/com/lcy/pojo/Type.java
+++ src/main/src/com/lcy/pojo/Type.java
@@ -1,9 +1,11 @@
 package com.lcy.pojo;
 
+import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.GenericGenerator;
 import pojo.ActiveAndDisplayOrder;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  * 类型、字典
@@ -29,7 +31,18 @@ public class Type extends ActiveAndDisplayOrder {
      */
     @Column(name = "IsLeaf")
     private boolean isLeaf;
-    private String ParentId;
+    /**一对多那方:设置懒加载*/
+    @OneToMany(mappedBy="parent",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OrderBy(value = "id ASC")/**指明加载OrderItem 时按id 的升序排序*/
+    private Set<Type> types;
+    @Column(name = "ParentId")
+    private String parentId;
+    /**级联刷新*/
+    /**optional表示该对象可有可无，它的值为true表示该外键可以为null，它的值为false表示该外键为not null*/
+    @ManyToOne(cascade=CascadeType.REFRESH,optional=false,fetch = FetchType.LAZY)
+    /**指定外键列(这也表示为所在对象为“关系被维护端”)*/
+    @JoinColumn(name = "parentId")
+    private Type parent;
 
     public String getId() {
         return id;
@@ -56,10 +69,26 @@ public class Type extends ActiveAndDisplayOrder {
     }
 
     public String getParentId() {
-        return ParentId;
+        return parentId;
     }
 
     public void setParentId(String parentId) {
-        ParentId = parentId;
+        parentId = parentId;
+    }
+
+    public Set<Type> getTypes() {
+        return types;
+    }
+
+    public void setTypes(Set<Type> types) {
+        this.types = types;
+    }
+
+    public Type getParent() {
+        return parent;
+    }
+
+    public void setParent(Type parent) {
+        this.parent = parent;
     }
 }
