cb4ab4e478df00db0c2c4edae5dfd440b9a5052c
梁城月 <2524499314@qq.com>
2018-04-25 11:21:12 +0800

    进入H+后台

---
+++ src/main/src/com/lcy/controller/CommonController.java
@@ -0,0 +1,17 @@
+package com.lcy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/common")
+public class CommonController {
+    @RequestMapping("/toFrame")
+    public String toFrame(){
+        return "backstage/frame";
+    }
+    @RequestMapping("/toLogin")
+    public String toLogin(){
+        return "backstage/login";
+    }
+}
